bfa6570acd6396cd9d571a6066939aca09850980
Steven Friedman <steven.rael.friedman@gmail.com>
2013-07-08 13:21:49 +0000

    Added web.js to read from file

--- web.js
+++ web.js
@@ -2,11 +2,20 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+var textout = fs.readFileSync("index.html", "utf-8");
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(textout);
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
